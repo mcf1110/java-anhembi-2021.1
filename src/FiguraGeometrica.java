091e5634ee4c1cfe6bd831220052ded0f4e8124e
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-05-06 20:38:20 -0300

    figuras

---
+++ src/FiguraGeometrica.java
@@ -0,0 +1,13 @@
+public abstract class FiguraGeometrica {
+    private String nome;
+
+    public FiguraGeometrica(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public abstract double calcularArea();
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá");
+        Circulo c = new Circulo("c1", 4);
+        Quadrado q = new Quadrado("q1", 3);
+        Triangulo t = new Triangulo("t1", 2, 3);
+
+        ArrayList<FiguraGeometrica> figuraGeometricas = new ArrayList<>();
+        figuraGeometricas.add(c);
+        figuraGeometricas.add(q);
+        figuraGeometricas.add(t);
+
+        for (int i = 0; i < figuraGeometricas.size(); i++) {
+            FiguraGeometrica f = figuraGeometricas.get(i);
+            System.out.println(f.getNome() + ": " + f.calcularArea());
+        }
     }
 }
